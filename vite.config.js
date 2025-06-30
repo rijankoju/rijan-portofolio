895694731a0eabb1dc503a31a811f574e0e7d686
kojurijan <rijan.koju@mandalabs.com>
2025-06-30 12:57:53 +0545

    adding cloudfare creds

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
+  plugins: [react()],
   build: {
-    outDir: 'build', // <- required for Cloudflare Pages config
-  }
+    outDir: 'build', // ← make sure it's 'build' for Cloudflare Pages
+  },
 })
